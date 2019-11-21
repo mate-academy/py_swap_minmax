b0b23501374a9a4fe0cfe7ffcd85092be1c43b0e
alexander <earthhuman41@gmail.com>
2019-11-22 01:25:47 +0200

    implemented swap_minmax function

--- swap_minmax.py
+++ swap_minmax.py
@@ -1,6 +1,31 @@
+"""
+swap_minmax
+"""
 from typing import List
 
 
-def swap_minmax(l: List[int]) -> List[int]:
+def swap_minmax(numbers: List[int]) -> List[int]:
+    """
+    function replace max number and min number by places
+    :param numbers: [int]
+    :return: [int]
+    """
+    if len(numbers) == 0:
+        return []
 
-    return l
+    max_number = numbers[0]
+    min_number = numbers[0]
+    index_min = 0
+    index_max = 0
+
+    for i, number in enumerate(numbers):
+        if number > max_number:
+            max_number = number
+            index_max = i
+        else:
+            min_number = number
+            index_min = i
+
+    numbers[index_max], numbers[index_min] = min_number, max_number
+
+    return numbers
