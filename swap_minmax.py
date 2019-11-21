928be9f8306351fd97ade85ed5327aad0f1b84af
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-21 12:01:02 +0200

    Hope my idea is not the worst one. I understand the logic of the task but I may not be aware of more beautiful instruments to process it.

--- swap_minmax.py
+++ swap_minmax.py
@@ -1,6 +1,28 @@
-from typing import List
+"""please explain when do we really need this docstring"""
+#from typing import List
 
+def swap_minmax(initial_sequence):
+    """docstring"""
+    if len(initial_sequence) <= 1:
+        return initial_sequence
+    numbers_sequence = initial_sequence
+    min_number = numbers_sequence[0]
+    index_min = 0
+    max_number = numbers_sequence[0]
+    index_max = 0
+    for number in numbers_sequence:
+        if number > max_number:
+            max_number = number
+            index_max = numbers_sequence.index(number)
+        if number < min_number:
+            min_number = number
+            index_min = numbers_sequence.index(number)
+    numbers_sequence[index_max] = "swap to min_number"
+    numbers_sequence[index_min] = "swap to max_number"
+    for seq, value in enumerate(numbers_sequence):
+        if value == "swap to min_number":
+            numbers_sequence[seq] = min_number
+        elif value == "swap to max_number":
+            numbers_sequence[seq] = max_number
 
-def swap_minmax(l: List[int]) -> List[int]:
-
-    return l
+    return numbers_sequence
