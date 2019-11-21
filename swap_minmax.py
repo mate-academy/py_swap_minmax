98c655007e99702b62dcd2ed6a48227aef003e55
dima <dima.sapsay@gmail.com>
2019-11-21 12:21:29 +0200

    first commit

--- swap_minmax.py
+++ swap_minmax.py
@@ -1,6 +1,29 @@
+"""
+Swapping minimal and maximal elements of a list
+"""
+
 from typing import List
 
 
-def swap_minmax(l: List[int]) -> List[int]:
+def swap_minmax(nums: List[int]) -> List[int]:
+    """ Swapping elements of a list """
+    if len(nums) <= 1:
+        return nums
 
-    return l
+    min_num, max_num = find_minmax(nums)
+    min_index = nums.index(min_num)
+    max_index = nums.index(max_num)
+    nums[min_index] = max_num
+    nums[max_index] = min_num
+    return nums
+
+
+def find_minmax(nums):
+    """ Func find min & max values """
+    min_num = max_num = nums[0]
+    for num in nums[1:]:
+        if min_num > num:
+            min_num = num
+        if max_num < num:
+            max_num = num
+    return min_num, max_num
