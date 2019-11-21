b6f0a3e36732a3853487f2a1294bcaf991ea379c
logotip <logotip123@yahoo.com>
2019-11-21 10:17:48 +0200

    v 1.0

--- swap_minmax.py
+++ swap_minmax.py
@@ -1,6 +1,23 @@
+"""Swap min max module"""
 from typing import List
 
 
-def swap_minmax(l: List[int]) -> List[int]:
-
-    return l
+def swap_minmax(values: List[int]) -> List[int]:
+    """Swap min max function"""
+    if len(values) < 2:
+        return values
+    minimal, maximum = 0, 0
+    minimal_index, maximum_index = 0, 0
+    for index, value in enumerate(values):
+        if index == 0:
+            minimal = value
+            maximum = value
+            continue
+        if value < minimal:
+            minimal = value
+            minimal_index = index
+        if value > maximum:
+            maximum = value
+            maximum_index = index
+    values[minimal_index], values[maximum_index] = values[maximum_index], values[minimal_index]
+    return values
