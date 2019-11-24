0618e61a6c38975a7c0719ce8bc8afe1973bc300
Platoniuks <platon@alterpost.org>
2019-11-24 13:49:14 +0200

    resolved

--- swap_minmax.py
+++ swap_minmax.py
@@ -1,6 +1,25 @@
+"""Create a function for swapping minimal
+and maximal elements of a list.
+Don't use built-in min()/max() functions.
+"""
+
 from typing import List
 
 
-def swap_minmax(l: List[int]) -> List[int]:
+def swap_minmax(nums: List[int]) -> List[int]:
+    """General function"""
+    if not nums:
+        return []
 
-    return l
+    smallest = nums[0]
+    biggest = 0
+
+    for num in nums:
+        if num < smallest:
+            smallest = num
+        elif num > biggest:
+            biggest = num
+    index_small = nums.index(smallest)
+    index_big = nums.index(biggest)
+    nums[index_small], nums[index_big] = biggest, smallest
+    return nums
